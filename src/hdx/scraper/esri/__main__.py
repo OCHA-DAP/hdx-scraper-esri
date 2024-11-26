9b673800dd4d80d207f5420747f5c973e1eaccf8
Briar <briar.j.mills@gmail.com>
2024-11-26 11:13:59 -0500

    Set up main and parameters

--- src/hdx/scraper/esri/__main__.py
+++ src/hdx/scraper/esri/__main__.py
@@ -5,11 +5,14 @@ script then creates in HDX.
 
 """
 
+import argparse
 import logging
+from os import getenv
 from os.path import dirname, expanduser, join
 
+from esri import Esri
 from hdx.api.configuration import Configuration
-from hdx.facades.infer_arguments import facade
+from hdx.facades.keyword_arguments import facade
 from hdx.utilities.downloader import Download
 from hdx.utilities.path import (
     wheretostart_tempdir_batch,
@@ -20,16 +23,43 @@ logger = logging.getLogger(__name__)
 
 _USER_AGENT_LOOKUP = "hdx-scraper-esri"
 _SAVED_DATA_DIR = "saved_data"  # Keep in repo to avoid deletion in /tmp
-_UPDATED_BY_SCRIPT = "HDX Scraper: esri"
+_UPDATED_BY_SCRIPT = "HDX Scraper: Esri"
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-un", "--username", default=None, help="Esri username")
+    parser.add_argument("-pw", "--password", default=None, help="Esri password")
+    parser.add_argument(
+        "-sv",
+        "--save",
+        default=False,
+        action="store_true",
+        help="Save downloaded data",
+    )
+    parser.add_argument(
+        "-usv",
+        "--use_saved",
+        default=False,
+        action="store_true",
+        help="Use saved data",
+    )
+    args = parser.parse_args()
+    return args
 
 
 def main(
+    username: str,
+    password: str,
     save: bool = True,
     use_saved: bool = False,
+    **ignore,
 ) -> None:
     """Generate datasets and create them in HDX
 
     Args:
+        username (str): Esri username
+        password (str): Esri password
         save (bool): Save downloaded data. Defaults to True.
         use_saved (bool): Use saved data. Defaults to False.
 
@@ -48,24 +78,30 @@ def main(
                 use_saved=use_saved,
             )
             configuration = Configuration.read()
-            #
-            # Steps to generate dataset
-            #
-            dataset.update_from_yaml(
-                path=join(
-                    dirname(__file__), "config", "hdx_dataset_static.yaml"
+            esri = Esri(configuration, retriever, username, password)
+            esri.list_layers()
+            for layer in esri.data:
+                dataset = esri.generate_dataset(layer)
+                dataset.update_from_yaml(
+                    path=join(dirname(__file__), "config", "hdx_dataset_static.yaml")
                 )
-            )
-            dataset.create_in_hdx(
-                remove_additional_resources=True,
-                match_resource_order=False,
-                hxl_update=False,
-                updated_by_script=_UPDATED_BY_SCRIPT,
-                batch=info["batch"],
-            )
+                dataset.create_in_hdx(
+                    remove_additional_resources=True,
+                    match_resource_order=False,
+                    hxl_update=False,
+                    updated_by_script=_UPDATED_BY_SCRIPT,
+                    batch=info["batch"],
+                )
 
 
 if __name__ == "__main__":
+    args = parse_args()
+    username = args.username
+    if username is None:
+        username = getenv("ESRI_USERNAME")
+    password = args.password
+    if password is None:
+        password = getenv("ESRI_PASSWORD")
     facade(
         main,
         hdx_site="dev",
@@ -73,4 +109,9 @@ if __name__ == "__main__":
         user_agent_lookup=_USER_AGENT_LOOKUP,
         project_config_yaml=join(
             dirname(__file__), "config", "project_configuration.yaml"
-    ))
+        ),
+        username=username,
+        password=password,
+        save=args.save,
+        use_saved=args.use_saved,
+    )
--- src/hdx/scraper/esri/esri.py
+++ src/hdx/scraper/esri/esri.py
@@ -2,29 +2,42 @@
 """esri scraper"""
 
 import logging
-from typing import List, Optional
+from typing import Optional
 
+import arcgis
 
 from hdx.api.configuration import Configuration
 from hdx.data.dataset import Dataset
-from hdx.data.hdxobject import HDXError
 from hdx.utilities.retriever import Retrieve
 
 logger = logging.getLogger(__name__)
 
 
-class esri:
-
+class Esri:
     def __init__(
-        self, configuration: Configuration, retriever: Retrieve, temp_dir: str
+        self,
+        configuration: Configuration,
+        retriever: Retrieve,
+        username: str,
+        password: str,
     ):
         self._configuration = configuration
         self._retriever = retriever
-        self._temp_dir = temp_dir
+        self._username = username
+        self._password = password
+        self.data = {}
 
+    def list_layers(self) -> None:
+        portal = arcgis.gis.GIS(
+            self._configuration["base_url"],
+            username=self._username,
+            password=self._password,
+        )
+        maps = portal.content.search(
+            query="type:Web Map", item_type="Web Map", max_items=1000
+        )
 
-    def generate_dataset(self) -> Optional[Dataset]:
-
+    def generate_dataset(self, layer: str) -> Optional[Dataset]:
         # To be generated
         dataset_name = None
         dataset_title = None
@@ -41,14 +54,10 @@ class esri:
         )
 
         dataset.set_time_period(dataset_time_period)
-        dataset.add_tagsa(dataset_tags)
+        dataset.add_tags(dataset_tags)
         # Only if needed
         dataset.set_subnational(True)
-        try:
-            dataset.add_country_location(dataset_country_iso3)
-        except HDXError:
-            logger.error(f"Couldn't find country {dataset_country_iso3}, skipping")
-            return
+        dataset.add_country_location(dataset_country_iso3)
 
         # Add resources here
 
